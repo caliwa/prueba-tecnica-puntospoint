70235f80e0c97d6b4f3f10f44cd58c5bcf326d39
caliwa <carlos_gonzalez82201@elpoli.edu.co>
2025-07-13 17:07:43 -0500

    get endpoints working for auth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -311,6 +311,10 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
+  config.warden do |manager|
+    manager.scope_defaults :user, store: false
+  end
+
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
     jwt.dispatch_requests = [
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get "pages/home"
-  get "pages/restricted"
+  get "current_user", to: 'current_user#index'
+  # get "pages/home"
+  # get "pages/restricted"
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
     registration: 'signup'
   },
+   
   controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
@@ -14,11 +16,11 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
   # root "posts#index"
