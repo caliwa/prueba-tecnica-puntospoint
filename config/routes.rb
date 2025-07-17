7d2fa42e40b8893ae4243bf84031b7533a3c1474
caliwa <carlos_gonzalez82201@elpoli.edu.co>
2025-07-17 12:59:31 -0500

    redirect default route to /api-docs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # require "sidekiq/web"
 Rails.application.routes.draw do
+  root to: redirect('/api-docs')
+
   get "up" => "rails/health#show", as: :rails_health_check
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
