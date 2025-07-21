76eb97484e226bfb8575bc534437537bad16e2f3
caliwa <carlos_gonzalez82201@elpoli.edu.co>
2025-07-20 22:24:06 -0500

    add default url opt prod emailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,11 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.default_url_options = {
+    protocol: 'https',
+    host: 'rubicon-prog.online'
+  }
+
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
